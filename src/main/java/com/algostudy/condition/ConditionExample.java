8913bbf800cd09f922951631e8c4a4efcac53509
yjink12 <yjin.k12@gmail.com>
2025-02-27 15:34:20 +0900

    first commit & 조건
     ## PR타입
    - [x] 기능 추가
    - [ ] 기능 수정
    - [ ] 기능 삭제
    - [ ] 버그 수정
    - [ ] 리팩토링, 코드 포맷팅
    - [ ] CI/CD, 환경변수
    - [ ] 문서 작업
    
     ## 내용
    조건 문제(백준)
    - 두수비교, 시험성적, 사분면 고르기, 윤년, 사파리월드
    
    resolves

---
+++ src/main/java/com/algostudy/condition/ConditionExample.java
@@ -0,0 +1,125 @@
+package com.algostudy.condition;
+
+import java.util.Scanner;
+
+/**
+ * packageName    : com.algostudy.condition
+ * fileName       : ConditionExample
+ * author         : admin
+ * date           : 2025-02-27
+ * description    : 조건
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-02-27        admin       최초 생성
+ */
+public class ConditionExample {
+  /**
+   *  백준 1330
+   *   - 두 수 비교하기
+   */
+  public void comparison() {
+    Scanner input = new Scanner(System.in);
+
+    int A = input.nextInt();
+    int B = input.nextInt();
+
+    input.close();
+
+    // 내 풀이
+    if (A>B) {
+      System.out.println(">");
+    } else if (A<B) {
+      System.out.println("<");
+    } else {
+      System.out.println("==");
+    }
+
+    // 좋은 풀이 -  삼항연산자 사용
+    String str = (A>B) ? ">" : ((A<B) ? "<" : "==");
+    System.out.println(str);
+  }
+
+  /**
+   *  백준 9498
+   *   - 시험 성적
+   */
+  public void examResult() {
+    Scanner input = new Scanner(System.in);
+
+    int result = input.nextInt();
+    input.close();
+
+    // 내 풀이
+    if (90 <= result) {
+      System.out.println("A");
+    } else if (80 <= result) {
+      System.out.println("B");
+    } else if (70 <= result) {
+      System.out.println("C");
+    } else if (60 <= result) {
+      System.out.println("D");
+    } else {
+      System.out.println("F");
+    }
+
+    // 좋은 풀이 -  삼항연산자 사용
+    System.out.println((result>= 90) ? "A" : (result >= 80)? "B" : (result >= 70)? "C" : (result>= 60) ? "D" : "F");
+  }
+
+  /**
+   *  백준 14681
+   *   - 사분면 고르기
+   */
+  public void quadrant() {
+    Scanner input = new Scanner(System.in);
+
+    int x = input.nextInt();
+    int y = input.nextInt();
+    input.close();
+
+    // 내 풀이
+    if (x>=0 && y>=0)
+      System.out.println(1);
+    else if (x<0 && y<0)
+      System.out.println(3);
+    else if (x<0 && y>0)
+      System.out.println(2);
+    else
+      System.out.println(4);
+  }
+
+  /**
+   *  백준 2753
+   *    - 윤년
+   */
+  public void leapYear() {
+    Scanner input = new Scanner(System.in);
+
+    int year = input.nextInt();
+    input.close();
+
+    // 내 풀이
+    if ((year%4 == 0 && year%100 != 0) || year%400 == 0)
+      System.out.println(1);
+    else
+      System.out.println(0);
+
+  }
+
+  /**
+   *  백준 2420
+   *    - 사파리월드
+   */
+  public void safariWorld() {
+    Scanner input = new Scanner(System.in);
+
+    long n = input.nextLong();
+    long m = input.nextLong();
+    input.close();
+
+    // 내 풀이
+    long diff = n>m ? (n-m) : (m-n);
+    System.out.println(diff);
+  }
+}
---
+++ src/main/java/com/algostudy/recursion/RecursionExample.java
@@ -0,0 +1,62 @@
+package com.algostudy.recursion;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * packageName    : com.algostudy.recursion
+ * fileName       : RecursionExample
+ * author         : admin
+ * date           : 2024-09-28-028
+ * description    : 순환 (Recursion)
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-09-28-028        admin       최초 생성
+ */
+public class RecursionExample {
+  public void sprout() {
+    System.out.println("         ,r\'\"7");
+    System.out.println("r`-_   ,\'  ,/");
+    System.out.println(" \\. \". L_r\'");
+    System.out.println("   `~\\/");
+    System.out.println("      |");
+    System.out.println("      |");
+  }
+//  public static int func(int n) {
+//    if (n == 0)
+//      return 0;
+//    else {
+//      return n + func(n-1);
+//    }
+//  }
+
+//  public static double power(double x, int n) {
+//    if (n==0)
+//      return 1;
+//    else
+//      return x * power(x, n-1);
+//  }
+//  public static int fibonacci(int n) {
+//    if (n<2)
+//      return n;
+//    else
+//      return fibonacci(n-1) + fibonacci(n-2);
+//  }
+  public static int gcd(int m, int n) {
+//    if (m<n) {
+//      // swap m and n;
+//      int tmp = m;
+//      m = n;
+//      n = tmp;
+//    }
+//    if (m%n == 0)
+//      return n;
+//    else
+//      return gcd(n, m%n);
+    if (n == 0)
+      return m;
+    else
+      return gcd(n, m%n);
+  }
+}
